c220ea464ba83e3318f9a66e4c7be2da1c3fc619
Dariss Harris <darissharris@gmail.com>
2020-10-20 09:18:55 -0700

    default works

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
+const { query, json } = require('express');
 
 const app = express();
 
 app.use(morgan('common')); // let's see what 'common' format looks like
 
+const appData = require('./playstore.js');
+
 app.get('/apps', (req, res) => {
-  // ALL OUR CODE HERE
+
+  const {sort , genres} = req.query;
+
+  let results = appData.filter(app =>
+    appData
+      .sort
+      .toLowerCase()
+      .includes(sort.toLowerCase())); 
+
+
+  //if statements
+
+  res
+    .json(appData);
+
 });
 
 app.listen(8000, () => {
